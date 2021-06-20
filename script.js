0ce464c0194a82b81441820292f30f461f841ff4
jas-sin82 <jasvir198@yahoo.co.in>
2021-06-20 19:22:19 +0000

    Add the main variable & ids

---
+++ script.js
@@ -0,0 +1,6 @@
+// all the main variables and id´s
+
+const startButton = document.getElementById("btn-start-quiz");
+const questionBox = document.getElementById("question-box");
+const quitQuiz = document.getElementById("quit-quiz");
+const nextQuestion = document.getElementById("next-question");
