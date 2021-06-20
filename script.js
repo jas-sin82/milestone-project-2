6c441c79d27fef728535a27c4e4add6c72b675cd
jas-sin82 <jasvir198@yahoo.co.in>
2021-06-20 19:32:33 +0000

    Create click function on start & quit buttons

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const startButton = document.getElementById("btn-start-quiz");
 const questionBox = document.getElementById("question-box");
 const quitQuiz = document.getElementById("quit-quiz");
 const nextQuestion = document.getElementById("next-question");
+
+// adding addEventfunction (when the start button clicked)
+
+startButton.addEventListener("click", function () {
+    questionBox.setAttribute("class", "question-containers");
+    $(".main-container").hide();
+})
+
+// adding addEventfunction (when the quit button clicked)
+
+quitQuiz.addEventListener("click", function () {
+    questionBox.classList.remove("question-containers");
+    questionBox.setAttribute("class", "quit-class");
+    $(".main-container").show();
+
+})
