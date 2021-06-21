10b30d23f3f4e43bef529966a38bcd7f49b344e9
jas-sin82 <jasvir198@yahoo.co.in>
2021-06-21 20:31:15 +0000

    Addevent function to click home page button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// all the main variables and id´s
+//  the main variables and id´s of home page and quiz box
 
 const startButton = document.getElementById("btn-start-quiz");
 const questionBox = document.getElementById("question-box");
@@ -202,19 +202,24 @@ function quizFinalResult() {
     if (totalScoreAchieved > 6) {
         finalScore.innerHTML =
             `<span>` + `Excellent You are rock ! <br> Please share your knowledge with <br> others to save our planet <br> <span>` + totalScoreAchieved + `</span> out of <span>` + questions.length + `</span> !</span>`;
-    }
-    else if (totalScoreAchieved > 4) {
+    } else if (totalScoreAchieved > 4) {
         finalScore.innerHTML =
             `<span>` + `Congratulation you got! <br><span>` + totalScoreAchieved + `</span> out of <span>` + questions.length + `</span> <br> Good Luck & Try Again  !</span>`;
-    }
-    else if (totalScoreAchieved < 3) {
-         finalScore.innerHTML =
+    } else if (totalScoreAchieved < 3) {
+        finalScore.innerHTML =
             `<span>` + ` Aho ! You Got <br> <span>` + totalScoreAchieved + `</span> Out Of <span>` + questions.length + `</span> <br> Good Luck & Try Again !</span>`;
-    }
-    else {
+    } else {
         finalScore.innerHTML =
             `<span>` + ` Aho ! You Got <br> <span>` + totalScoreAchieved + `</span> Out Of <span>` + questions.length + `</span> <br> Good Luck & Try Again  !</span>`;
     }
 
 }
 
+// restart button function when user will click it brings user back to Home page ...
+
+restartButton.addEventListener("click", function () {
+    restartQuiz.classList.remove("restart_quiz");
+    $(".main-container").show();
+    restartQuiz.setAttribute("class", "quit-class");
+    window.location.reload();
+})
